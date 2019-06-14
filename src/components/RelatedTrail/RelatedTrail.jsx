51062c5eaa24a9ae6dde85e52d48aeb8c703a26c
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2019-06-14 13:15:31 -0500

    styling tweak

--- src/components/RelatedTrail/RelatedTrail.jsx
+++ src/components/RelatedTrail/RelatedTrail.jsx
@@ -4,7 +4,13 @@ import styles from './RelatedTrail.module.scss';
 
 const RelatedTrail = () => {
   return (
-    <div className={styles.component}></div>
+    <div className={styles.component}>
+      <div className={styles.userName}>
+        trail name
+      </div>
+
+      <div className={styles.card}></div>
+    </div>
   );
 };
 
